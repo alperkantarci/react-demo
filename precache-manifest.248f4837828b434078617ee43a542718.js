6390987205c3816f326a95cb2f68821004d56a1f
alperk <alperkantrc@gmail.com>
2019-07-10 20:52:19 +0300

    Updates

---
+++ precache-manifest.248f4837828b434078617ee43a542718.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6559cd85fb4842b1e3c7edde944652ff",
+    "url": "/react-demo/index.html"
+  },
+  {
+    "revision": "41d7d0f471eb122a851c",
+    "url": "/react-demo/static/css/2.0af631bd.chunk.css"
+  },
+  {
+    "revision": "4f44e14f0405d0bff25e",
+    "url": "/react-demo/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "41d7d0f471eb122a851c",
+    "url": "/react-demo/static/js/2.590933e2.chunk.js"
+  },
+  {
+    "revision": "4f44e14f0405d0bff25e",
+    "url": "/react-demo/static/js/main.d591dd25.chunk.js"
+  },
+  {
+    "revision": "78404b4b0f010a1ef451",
+    "url": "/react-demo/static/js/runtime~main.3dcdfef1.js"
+  }
+]);
